09521286732b72ea77acb414bd1c0daad230245a
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-07-13 11:59:09 +0300

    Add  solution for problem 1 in level 5

---
+++ 05 - Algorithms & Problem-Solving Level 2/Problem_#1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void PrintTableHeader() {
+    cout << "\n\n\t\t\t Multiplication Table From 1 to 10\n\n";
+    cout << "\t";
+    for (int i = 1; i <= 10; i++) {
+        cout << i << "\t";
+    }
+    cout << "\n___________________________________________________________________________________\n";
+}
+
+string ColumnSeparator(int i) {
+    if (i < 10)
+        return "   |";
+    else
+        return "  |";
+}
+
+void PrintMultiplicationTable() {
+    PrintTableHeader();
+    for (int i = 1; i <= 10; i++) {
+        cout << " " << i << ColumnSeparator(i) << "\t";
+        for (int j = 1; j <= 10; j++) {
+            cout << i * j << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    PrintMultiplicationTable();
+    return 0;
+}
---
+++ 05 - Algorithms & Problem-Solving Level 2/README.md
@@ -0,0 +1,1 @@
+Solve Prolems level 5 in Programming advices
