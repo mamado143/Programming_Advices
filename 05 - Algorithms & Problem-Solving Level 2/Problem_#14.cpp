96059ef94d4984b243b62e5d28324cb4a3fe9780
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-08-06 14:18:14 +0300

    solve prolem 14 15 level 5

--- 05 - Algorithms & Problem-Solving Level 2/Problem_#14.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/Problem_#14.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /**
- * Read_number -  Reads a positive number and print inverted.
+ * Read_number -  Reads a positive number and print inverted letter.
  * @message: The message to display when asking the user for input.
  * Return: The positive number entered by the user.
  */
@@ -19,19 +19,22 @@ int Read_number(string message)
     return number;
 }
 /**
- * PrintNumberPattern -  Prints the  number pattern in a diamond way.
+ * PrintNumberPattern -  Prints the  PrintInvertedAsciPattern in a diamond way.
  * @number: The number representing the highest value in the pattern.
  */
-void PrintNumberPattern(int number)
+void PrintInvertedAsciPattern(int number)
 {
     int i, j;
     cout << "\n";
 
-    for (i = 1; i < number; i++)
+    for (i = number; i >= 1; i--)
     {
+        char ch = 'A';
+
         for (j = 0; j < i; j++)
         {
-            cout << i;
+            cout << ch;
+            ch++;
         }
         cout << "\n";
     }
@@ -44,7 +47,6 @@ void PrintNumberPattern(int number)
 int main()
 {
     int number = Read_number("Enter the number");
-    PrintNumberPattern(number);
+    PrintInvertedAsciPattern(number);
     return 0;
 }
-
