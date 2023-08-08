a640a467d1e6a36ecd28fbb7aeee5fedc4966e3a
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2023-08-08 22:54:22 +0300

    solve prolem 22level 5

---
+++ 05 - Algorithms & Problem-Solving Level 2/Problem_#22.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/**
+  * @breif - Write a program to read N elements and store them in array then print
+  * all array elements and ask for the number to check, then print how many nunber 
+  *
+  */
+int read_elements()
+{
+	int num;
+
+	cin>>num;
+	cout<<"Enter array elements:\n";
+	return num;
+}
+int main ()
+{
+	read_elements();
+	return 0;
+}
